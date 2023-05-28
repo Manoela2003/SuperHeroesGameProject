5a0e0a5c3fc188ee2e5e9a91d7da22a0dff79ae5
Manoela2003 <manoelabarakova03@gmail.com>
2023-05-28 22:58:23 +0300

    Added functions for logging in and added variable for index of logged in player

--- SuperHeroesGameProject/SuperHeroesGame.cpp
+++ SuperHeroesGameProject/SuperHeroesGame.cpp
@@ -1,5 +1,35 @@
 #include "SuperHeroesGame.h"
 
+bool SuperHeroesGame::LogInAsAdministrator(const char* username, const char* password) const {
+	if (!username || !password)
+		throw std::invalid_argument("The username or password doesn't exist");
+
+	for (int i = 0; i < administrators.GetCount(); i++) {
+		if (!strcmp(administrators[i]->username.c_str(), username)) {
+			if (!strcmp(administrators[i]->password.c_str(), password)) {
+				IndexOfloggedInAdmin = i;
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+bool SuperHeroesGame::LogInAsPlayer(const char* username, const char* password) const{
+	if (!username || !password)
+		throw std::invalid_argument("The username or password doesn't exist");
+
+	for (int i = 0; i < players.GetCount(); i++) {
+		if (!strcmp(players[i]->username.c_str(), username)) {
+			if (!strcmp(players[i]->password.c_str(), password)) {
+				IndexOfloggedInPlayer = i;
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
 bool SuperHeroesGame::AddAdministrator(const Administrator& other) {
 	if (IndexOfloggedInAdmin != -1) {
 		administrators.AddElement(new Administrator(other));
@@ -30,4 +60,21 @@ bool SuperHeroesGame::DeletePlayer(const char* username) {
 		}
 	}
 	return false;
+}
+
+bool SuperHeroesGame::DeleteOwnProfile() {
+	if (IndexOfloggedInPlayer != -1) {
+		players.RemoveElement(IndexOfloggedInPlayer);
+		IndexOfloggedInPlayer = -1;
+		return true;
+	}
+	return false;
+}
+
+bool SuperHeroesGame::IsAdministratorLogged() const{
+	return IndexOfloggedInAdmin != -1;
+}
+
+bool SuperHeroesGame::IsPlayerLogged() const{
+	return IndexOfloggedInPlayer != -1;
 }
--- SuperHeroesGameProject/SuperHeroesGame.h
+++ SuperHeroesGameProject/SuperHeroesGame.h
@@ -1,22 +1,26 @@
 #pragma once
-#include "Vector.hpp"
 #include "Administrator.h"
 #include "Player.h"
-#include "SharedPtr.hpp"
 
 class SuperHeroesGame {
 private:
 	Vector<SharedPtr<Administrator>> administrators;
 	Vector<SharedPtr<Player>> players;
 
-	unsigned IndexOfloggedInAdmin = -1;
+	mutable unsigned IndexOfloggedInAdmin = -1;
+	mutable unsigned IndexOfloggedInPlayer = -1;
 
 public:
 	SuperHeroesGame() = default;
 
+	bool LogInAsAdministrator(const char* username, const char* password) const;
+	bool LogInAsPlayer(const char* username, const char* password) const;
 	bool AddAdministrator(const Administrator& other);
 	bool AddPlayer(const Player& other);
 	bool DeletePlayer(const char* username);
+	bool DeleteOwnProfile();
 
+	bool IsAdministratorLogged() const;
+	bool IsPlayerLogged() const;
 };
 
