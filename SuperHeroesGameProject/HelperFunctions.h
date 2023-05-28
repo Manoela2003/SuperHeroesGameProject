d46edcdf9fdfb038619cd30b3c65a8416c032f58
Manoela2003 <manoelabarakova03@gmail.com>
2023-05-28 14:56:59 +0300

    Added file with helper functions

---
+++ SuperHeroesGameProject/HelperFunctions.h
@@ -0,0 +1,75 @@
+#pragma once
+#include <cstring>
+#include <stdexcept>
+
+bool ContainsDigits(const char* name) {
+	for (int i = 0; i < strlen(name); i++) {
+		if (name[i] >= '0' && name[i] <= '9')
+			return true;
+	}
+	return false;
+}
+
+bool ContainsLetters(const char* name) {
+	for (int i = 0; i < strlen(name); i++) {
+		if ((name[i] >= 'a' && name[i] <= 'z') || (name[i] >= 'A' && name[i] <= 'Z'))
+			return true;
+	}
+	return false;
+}
+
+bool ContainsAsperand(const char* name) {
+	for (int i = 0; i < strlen(name); i++) {
+		if (name[i] == '@')
+			return true;
+	}
+	return false;
+}
+
+bool ContainsSpecialSymbols(const char* name) {
+	for (int i = 0; i < strlen(name); i++) {
+		if (name[i] == '@' || name[i] == '#' || name[i] == '-' || name[i] == '_')
+			return true;
+	}
+	return false;
+}
+
+bool StartsWithCapitalLetter(const char* name) {
+	return (name[0] >= 'A' && name[0] <= 'Z') ? true : false;
+}
+
+bool IsEmailValid(const char* email) {
+	if (!email)
+		throw std::invalid_argument("The email doesn't exist");
+
+	if(!ContainsAsperand(email))
+		throw std::logic_error("Not a valid email!. It must contains '@'");
+}
+
+void IsNameValid(const char* name) {
+	if (!name)
+		throw std::invalid_argument("The name doesn't exist");
+
+	if (!StartsWithCapitalLetter(name))
+		throw std::logic_error("The name must start with capital letter");
+
+	if (ContainsDigits(name))
+		throw std::logic_error("The name cannot contain digits");
+}
+
+void IsPasswordValid(const char* pass) {
+	if (!pass)
+		throw std::invalid_argument("The password doesn't exist");
+
+	if (strlen(pass) < 8)
+		throw std::logic_error("The password must be at least 8 characters long");
+
+	if (!ContainsDigits(pass))
+		throw std::logic_error("The password must contains at least one digit");
+
+	if (!ContainsLetters(pass))
+		throw std::logic_error("The password must contains at least one letter");
+
+	if (!ContainsSpecialSymbols(pass))
+		throw std::logic_error("The password must contains at least one special symbol: '@', '#', '-', '_'");
+}
