bfde7f1ea2cd01fda47e97eb622f27f85d7c464f
Manoela2003 <manoelabarakova03@gmail.com>
2023-06-14 12:57:50 +0300

    Added functions for adding SuperHero and printing

---
+++ SuperHeroesGameProject/Shop.cpp
@@ -0,0 +1,46 @@
+#include "Shop.h"
+
+static void PrintDashes(int dashesCount) {
+	for (int i = 0; i < dashesCount; i++)
+		std::cout << '-';
+}
+
+bool Shop::IsShopEmpty() const {
+	return superHeroes.GetCount() == 0;
+}
+
+void Shop::AddNewSuperHero(const char* firstName, const char* lastName, const char* nickname, const char* power, int strength, double price) {
+	superHeroes.AddElement(new SuperHero(firstName, lastName, nickname, power, strength, price));
+}
+
+bool Shop::PrintSuperHeroes() const {
+	if (IsShopEmpty())
+		return false;
+
+	for (int i = 0; i < superHeroes.GetCount(); i++) {
+		superHeroes[i]->PrintDetailedInfo();
+		PrintDashes(15);
+	}
+	return true;
+}
+
+void Shop::RemoveSuperHero(const SuperHero& hero){
+	for (int i = 0; i < superHeroes.GetCount(); i++) {
+		if (!strcmp(superHeroes[i]->GetNickname(), hero.GetNickname())) {
+			superHeroes.RemoveElement(i);
+			return;
+		}
+	}
+}
+
+SuperHero Shop::FindSuperHero(const char* nickname) const{
+	if (nickname == nullptr)
+		throw std::invalid_argument("The nickname doesn't exist");
+
+	for (int i = 0; i < superHeroes.GetCount(); i++) {
+		if (!strcmp(nickname, superHeroes[i]->GetNickname()))
+			return *superHeroes[i];
+	}
+
+	throw std::logic_error("There is no such SuperHero");
+}
---
+++ SuperHeroesGameProject/Shop.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "SuperHero.h"
+#include "Vector.hpp"
+#include "SharedPtr.hpp"
+
+class Shop{
+private:
+	Vector<SharedPtr<SuperHero>> superHeroes;
+
+public:
+	bool IsShopEmpty() const;
+	void AddNewSuperHero(const char* firstName, const char* lastName, const char* nickname, const char* power, int strength, double price);
+	bool PrintSuperHeroes() const;
+	void RemoveSuperHero(const SuperHero& hero);
+	SuperHero FindSuperHero(const char* nickname) const;
+};
+
