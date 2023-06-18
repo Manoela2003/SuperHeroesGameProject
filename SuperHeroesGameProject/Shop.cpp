67ff8a7cb79b172c1c299e9394e1d07b60ca7dec
Manoela2003 <manoelabarakova03@gmail.com>
2023-06-18 20:34:01 +0300

    Added functionality for storing deleted Superheroes

--- SuperHeroesGameProject/Shop.cpp
+++ SuperHeroesGameProject/Shop.cpp
@@ -1,9 +1,13 @@
 #include "Shop.h"
-#include "FileFunctions.h"
 
 static void PrintDashes(int dashesCount) {
 	for (int i = 0; i < dashesCount; i++)
 		std::cout << '-';
+	std::cout << std::endl;
+}
+
+int Shop::GetCountOfOldHeroes() const{
+	return oldHeroes.GetCount();
 }
 
 int Shop::GetCount() const{
@@ -14,8 +18,16 @@ bool Shop::IsShopEmpty() const {
 	return superHeroes.GetCount() == 0;
 }
 
+void Shop::AddNewSuperHero(SuperHero&& other){
+	superHeroes.AddElement(new SuperHero(std::move(other)));
+}
+
 void Shop::AddNewSuperHero(const char* firstName, const char* lastName, const char* nickname, const char* power, int strength, double price) {
 	superHeroes.AddElement(new SuperHero(firstName, lastName, nickname, power, strength, price));
+}
+
+void Shop::AddOldSuperHero(const SuperHero& other){
+	oldHeroes.AddElement(new SuperHero(other));
 }
 
 bool Shop::PrintSuperHeroes() const {
@@ -24,6 +36,18 @@ bool Shop::PrintSuperHeroes() const {
 
 	for (int i = 0; i < superHeroes.GetCount(); i++) {
 		superHeroes[i]->PrintDetailedInfo();
+		PrintDashes(15);
+	}
+	return true;
+}
+
+bool Shop::PrintOldSuperHeroes() const{
+	if (oldHeroes.GetCount() == 0)
+		return false;
+
+	for (int i = 0; i < oldHeroes.GetCount(); i++) {
+		std::cout << i + 1 << ". ";
+		oldHeroes[i]->PrintDetailedInfo();
 		PrintDashes(15);
 	}
 	return true;
@@ -50,6 +74,31 @@ SuperHero Shop::FindSuperHero(const char* nickname) const{
 	throw std::logic_error("There is no such SuperHero");
 }
 
+SuperHero& Shop::GetOldSuperHero(int index){
+	if (index < 0 || index > oldHeroes.GetCount())
+		throw std::out_of_range("Out of range");
+
+	return *oldHeroes[index];
+}
+
+void Shop::RemoveOldSuperHero(int index){
+	if (index < 0 || index > oldHeroes.GetCount())
+		throw std::out_of_range("Out of range");
+
+	oldHeroes.RemoveElement(index);
+}
+
+int Shop::IndexOfSuperHero(const char* nickname) const{
+	if (nickname == nullptr)
+		throw std::invalid_argument("The nickname doesn't exist");
+
+	for (int i = 0; i < superHeroes.GetCount(); i++) {
+		if (!strcmp(nickname, superHeroes[i]->GetNickname()))
+			return i;
+	}
+	return -1;
+}
+
 void Shop::ReadFromFile(std::ifstream& file){
 	unsigned count = 0;
 	file.read((char*)&count, sizeof(count));
@@ -58,9 +107,25 @@ void Shop::ReadFromFile(std::ifstream& file){
 		hero.ReadFromFile(file);
 		superHeroes.AddElement(new SuperHero(std::move(hero)));
 	}
+	file.read((char*)&count, sizeof(count));
+	for (int i = 0; i < count; i++) {
+		SuperHero hero;
+		hero.ReadFromFile(file);
+		oldHeroes.AddElement(new SuperHero(std::move(hero)));
+	}
 }
 
 void Shop::SaveToFile(std::ofstream& file) const{
-	for (int i = 0; i < superHeroes.GetCount(); i++)
+	unsigned count = GetCount();
+
+	file.write((const char*)&count, sizeof(count));
+
+	for (int i = 0; i < count; i++)
 		superHeroes[i]->SaveToFile(file);
+
+	count = GetCountOfOldHeroes();
+	file.write((const char*)&count, sizeof(count));
+
+	for (int i = 0; i < count; i++)
+		oldHeroes[i]->SaveToFile(file);
 }
--- SuperHeroesGameProject/Shop.h
+++ SuperHeroesGameProject/Shop.h
@@ -6,14 +6,23 @@
 class Shop {
 private:
 	Vector<SharedPtr<SuperHero>> superHeroes;
+	Vector<SharedPtr<SuperHero>> oldHeroes;
 
 public:
+
+	int GetCountOfOldHeroes() const;
 	int GetCount() const;
 	bool IsShopEmpty() const;
+	void AddNewSuperHero(SuperHero&& other);
 	void AddNewSuperHero(const char* firstName, const char* lastName, const char* nickname, const char* power, int strength, double price);
+	void AddOldSuperHero(const SuperHero& other);
 	bool PrintSuperHeroes() const;
+	bool PrintOldSuperHeroes() const;
 	void RemoveSuperHero(const SuperHero& hero);
 	SuperHero FindSuperHero(const char* nickname) const;
+	SuperHero& GetOldSuperHero(int index);
+	void RemoveOldSuperHero(int index);
+	int IndexOfSuperHero(const char* nickname) const;
 
 	void ReadFromFile(std::ifstream& file);
 	void SaveToFile(std::ofstream& file) const;
