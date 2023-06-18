93ec4c54a381b70d08d8f9e47baa8d7c171c79ae
Manoela2003 <manoelabarakova03@gmail.com>
2023-06-18 20:31:52 +0300

    Added paths to files and periodical money

--- SuperHeroesGameProject/GlobalConstants.h
+++ SuperHeroesGameProject/GlobalConstants.h
@@ -4,7 +4,11 @@ const unsigned player_Starting_Balance = 1000;
 const unsigned buffer_Max_Size = 60;
 const unsigned win_Defense_Money = 400;
 const unsigned lose_Attack_Money = 500;
+const unsigned win_NoHeroes_Money = 200;
+const unsigned periodically_Won_Money = 300;
 
 const char adminFile[] = "administrators.dat";
 const char playerFile[] = "players.dat";
 const char shopFile[] = "shop.dat";
+const char loggedInFile[] = "loggedIn.dat";
+const char rankingFile[] = "ranking.txt";
